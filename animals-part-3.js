ed9ffd567915858b5cc0c720d67f1ecd964e21b6
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 13:30:03 -0500

    Create animals-part-3.js

---
+++ animals-part-3.js
@@ -0,0 +1,9 @@
+export const arcticAnimals = [
+  { name: "Arctic Hare", color: "white", biome: "arctic", evolveScore: 0 },
+  { name: "Penguin", color: "black", biome: "arctic", evolveScore: 5 },
+  { name: "Seal", color: "gray", biome: "arctic", evolveScore: 15 },
+  { name: "Walrus", color: "#A0522D", biome: "arctic", evolveScore: 30 },
+  { name: "Polar Bear", color: "white", biome: "arctic", evolveScore: 50 },
+  { name: "Yeti", color: "#E0FFFF", biome: "arctic", evolveScore: 100 },
+  { name: "Mammoth", color: "#70543E", biome: "arctic", evolveScore: 150 },
+];

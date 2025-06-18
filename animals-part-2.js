c7572a17b0e2d4b20e802f6bfe7ac95f9b736a42
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 13:29:38 -0500

    Create animals-part-2.js

---
+++ animals-part-2.js
@@ -0,0 +1,11 @@
+export const oceanAnimals = [
+  { name: "Trout", color: "#00BFFF", biome: "ocean", evolveScore: 0 },
+  { name: "Piranha", color: "red", biome: "ocean", evolveScore: 5 },
+  { name: "Tuna", color: "blue", biome: "ocean", evolveScore: 15 },
+  { name: "Swordfish", color: "steelblue", biome: "ocean", evolveScore: 30 },
+  { name: "Shark", color: "#808080", biome: "ocean", evolveScore: 50 },
+  { name: "Orca", color: "black", biome: "ocean", evolveScore: 100 },
+  { name: "Blue Whale", color: "#4682B4", biome: "ocean", evolveScore: 150 },
+  { name: "Giant Squid", color: "purple", biome: "ocean", evolveScore: 200 },
+  { name: "Kraken", color: "darkviolet", biome: "ocean", evolveScore: 300 },
+];

1b0065ea8f952ae7c66fd57efef024165df43e1a
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 13:24:07 -0500

    Create animals.js

---
+++ animals.js
@@ -0,0 +1,18 @@
+// animals.js
+
+const animals = [
+  // Level 0
+  { name: "Mouse", color: "gray", evolveScore: 0 },
+  { name: "Rabbit", color: "pink", evolveScore: 5 },
+  { name: "Pig", color: "brown", evolveScore: 15 },
+  { name: "Deer", color: "tan", evolveScore: 30 },
+  { name: "Fox", color: "orange", evolveScore: 50 },
+  { name: "Wolf", color: "lightgray", evolveScore: 70 },
+  { name: "Lion", color: "goldenrod", evolveScore: 100 },
+  { name: "Bear", color: "darkbrown", evolveScore: 130 },
+  { name: "Crocodile", color: "green", evolveScore: 170 },
+  { name: "Rhino", color: "gray", evolveScore: 220 },
+  { name: "Elephant", color: "lightgray", evolveScore: 280 },
+  { name: "Mammoth", color: "brown", evolveScore: 350 },
+  { name: "Dragon", color: "red", evolveScore: 450 },
+];

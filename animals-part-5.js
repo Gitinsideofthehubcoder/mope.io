4342eddb280cdc63b472bf18efbe583f932f7a05
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 13:30:39 -0500

    Create animals-part-5.js

---
+++ animals-part-5.js
@@ -0,0 +1,7 @@
+export const mythicalAnimals = [
+  { name: "Phoenix", color: "gold", biome: "mythical", evolveScore: 500 },
+  { name: "Dragon", color: "red", biome: "mythical", evolveScore: 600 },
+  { name: "Kraken", color: "purple", biome: "mythical", evolveScore: 650 },
+  { name: "Hydra", color: "darkgreen", biome: "mythical", evolveScore: 700 },
+  { name: "Griffin", color: "goldenrod", biome: "mythical", evolveScore: 750 },
+];

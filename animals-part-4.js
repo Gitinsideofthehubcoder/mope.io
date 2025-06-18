3381ecf2a6864a93bf9d37ded0ffa67ad33299f8
Gitinsideofthehubcoder <bankspoopy040@gmail.com>
2025-06-18 13:30:21 -0500

    Create animals-part-4.js

---
+++ animals-part-4.js
@@ -0,0 +1,9 @@
+export const desertAnimals = [
+  { name: "Meerkat", color: "sandybrown", biome: "desert", evolveScore: 0 },
+  { name: "Lizard", color: "green", biome: "desert", evolveScore: 5 },
+  { name: "Fennec Fox", color: "wheat", biome: "desert", evolveScore: 15 },
+  { name: "Hyena", color: "#8B4513", biome: "desert", evolveScore: 30 },
+  { name: "Camel", color: "#DEB887", biome: "desert", evolveScore: 50 },
+  { name: "Vulture", color: "brown", biome: "desert", evolveScore: 100 },
+  { name: "Mummy", color: "#EEE8AA", biome: "desert", evolveScore: 150 },
+];
